b55384cb54e7c132c5d6d0aeca4821e62b0448bc
kevin le <le_k@qwasar.io>
2021-06-25 06:14:49 +0000

    finished

---
+++ ex03/my_first_function.js
@@ -0,0 +1,7 @@
+
+// Following function my_first_function() will be a function that will print using console.log("my_first_function")
+console.log("my_first_function")
+my_first_function
+my_first_function
+
+my_first_function();
